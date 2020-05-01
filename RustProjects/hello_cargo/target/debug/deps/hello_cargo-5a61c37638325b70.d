59b29e303834184548d9a083b8db695eee5e9b7c
Samuel Feye <samfeye@gmail.com>
2020-05-01 15:55:13 -0500

    Add Hello Cargo project

---
+++ RustProjects/hello_cargo/target/debug/deps/hello_cargo-5a61c37638325b70.d
@@ -0,0 +1,5 @@
+/Users/samfeye/CS/RustProjects/hello_cargo/target/debug/deps/hello_cargo-5a61c37638325b70: src/main.rs
+
+/Users/samfeye/CS/RustProjects/hello_cargo/target/debug/deps/hello_cargo-5a61c37638325b70.d: src/main.rs
+
+src/main.rs:
---
+++ RustProjects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/samfeye/CS/RustProjects/hello_cargo/target/debug/hello_cargo: /Users/samfeye/CS/RustProjects/hello_cargo/src/main.rs
