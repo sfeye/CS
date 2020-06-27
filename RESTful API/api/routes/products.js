2ee916417758657f50379091ca70504407f36a77
Samuel Feye <samfeye@gmail.com>
2020-06-27 18:01:17 -0500

    Taking a break

--- RESTful API/api/routes/products.js
+++ RESTful API/api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,10 +11,26 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
+    });
+
+    product
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Handling POST requests to /products',
+            product: result
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
+
     res.status(201).json({
         message: 'Handling POST requests to /products',
         product: product
@@ -20,17 +39,16 @@ router.post('/', (req, res, next) => {
 
 router.get('/:productID', (req, res, next) => {
     const id = req.params.productID;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered the special id',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an id...',
-            id: id
-        });
-    }
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log("From database", doc);
+        res.status(200).json(doc);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
 });
 
 router.patch('/:productID', (req, res, next) => {
--- RESTful API/app.js
+++ RESTful API/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://sfeye:' 
++ process.env.MONGO_ATLAS_PW + 
+'@node-rest-6k4u6.mongodb.net/product?retryWrites=true&w=majority');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
